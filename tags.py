98078e503cbc9a9f4e919d82497c67ab16a6872e
Holt Hunter <holt@hunterhayven.com>
2014-06-27 18:29:41 -0500

    Fixed in user_link

--- tags.py
+++ tags.py
@@ -5,7 +5,7 @@ def user_link( username ):
 	""" Returns a string containing a link to the
 	user's profile page """
         user = User.get_by_name(username)
-        if user:
+        if user and user.last_seen:
             cls = ""
             delta = (datetime.now() - user.last_seen).seconds
             if delta < 300: # 5 minutes
@@ -13,5 +13,7 @@ def user_link( username ):
             if delta < 60:  # 1 minute
                 cls = 'class=veryrecent'
             return '<a %s href="www.shopmondays.com/user/%s">%s</a>' % (cls, username, username)
+        elif user:
+            return '<a href="www.shopmondays.com/user/%s">%s</a>' % (username, username)
         else:
             return username
