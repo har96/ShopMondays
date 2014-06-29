ee271e5e238bba80b2fec7e052ab0ee6ba2b9019
Hunter, Harrison <harrison.h.hunter@erdc.dren.mil>
2014-06-28 20:36:18 -0500

    Add lib/packages to configure_path

--- configure_path.py
+++ configure_path.py
@@ -3,4 +3,5 @@ import sys
 libraries = ['lib/packages/Markdown-2.4.1-py2.7.egg',
              'lib/packages/cffi-0.8.2-py2.7-macosx-10.9-intel.egg',
              'lib/packages/py_bcrypt',
-             'lib/packages/recaptcha']
+             'lib/packages/recaptcha',
+             'lib/packages']
